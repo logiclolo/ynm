515a88384c138448378e203e993cba0bfd175009
klaymen <klaymen@7a6c5977-b700-0410-9ebf-f0490b269a9c>
2016-03-15 06:01:11 +0000

    test_audio.py: move helper into test case function
    git-svn-id: http://rd1-1/subversion/SI/Tools/ynm/app/trunk@184151 7a6c5977-b700-0410-9ebf-f0490b269a9c

--- robot/all/test_audio.py
+++ robot/all/test_audio.py
@@ -5,41 +5,41 @@ from time import sleep
 from YNM.camera.frame.filter import Filter, audio_filter, CodecFilter
 
 
-def helper_general_codec_bitrate_change_and_match(streaming,
-                                                  configer, codec, bitrates):
-    supported_bitrates = bitrates
-
-    # enable audio and set codec type
-    configer.set('audioin_c0_mute=0&audioin_c0_s0_codectype=%s' % codec)
-
-    for bitrate in supported_bitrates:
-        # currently all products support only 1 audio stream
-        # we just simply change audioin_c0_s0_<codec>_bitrate
-        configer.set('audioin_c0_s0_%s_bitrate=%d' % (codec, bitrate))
-        sleep(2)
-
-        streaming.connect()
-
-        measure = Filter('measure')
-        audio_codec = CodecFilter(codec)
-        audio_measure = audio_filter + audio_codec + measure
-
-        for i in range(0, 100):
-            streaming.get_one_frame(audio_measure)
-
-        rate_config = float(bitrate)
-        rate_measure = float(measure.bitrate)
-        assert rate_config * 0.95 <= rate_measure <= rate_config * 1.05, \
-            "%s audio bitrate doesn't match. Exp %d, Act %f" % (codec, bitrate,
-                                                                measure.bitrate)
-
-        streaming.disconnect()
-
-
+@pytest.mark.audio
 def test_all_audio_codec_bitrate_change_and_match(streaming, configer):
 
     # Test camera bitrate by ALL audio codec type that could be changed
     # successfully and match expected bitrate
+
+    def helper_general_codec_bitrate_change_and_match(streaming,
+                                                    configer, codec, bitrates):
+        supported_bitrates = bitrates
+
+        # enable audio and set codec type
+        configer.set('audioin_c0_mute=0&audioin_c0_s0_codectype=%s' % codec)
+
+        for bitrate in supported_bitrates:
+            # currently all products support only 1 audio stream
+            # we just simply change audioin_c0_s0_<codec>_bitrate
+            configer.set('audioin_c0_s0_%s_bitrate=%d' % (codec, bitrate))
+            sleep(2)
+
+            streaming.connect()
+
+            measure = Filter('measure')
+            audio_codec = CodecFilter(codec)
+            audio_measure = audio_filter + audio_codec + measure
+
+            for i in range(0, 100):
+                streaming.get_one_frame(audio_measure)
+
+            rate_config = float(bitrate)
+            rate_measure = float(measure.bitrate)
+            assert rate_config * 0.95 <= rate_measure <= rate_config * 1.05, \
+                "%s audio bitrate doesn't match. Exp %d, Act %f" % (codec, bitrate,
+                                                                    measure.bitrate)
+
+            streaming.disconnect()
 
     codec_bitrates = {
         'aac4': [16000, 32000, 48000, 64000, 96000, 128000],
@@ -84,5 +84,3 @@ def test_audio_codec_c0_s0_change_ok(cam, streaming, configer):
             assert codec_filter.filter(f), "Codec change failed. Expected %s,\
                 actual %s" % (codec, f.codec)
         streaming.disconnect()
-
-
