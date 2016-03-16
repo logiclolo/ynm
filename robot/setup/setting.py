dac73986b9ea4b6179228433dd3bef2cc451d191
klaymen <klaymen@7a6c5977-b700-0410-9ebf-f0490b269a9c>
2016-03-16 02:07:39 +0000

    setting.py: adding platform keyword
    git-svn-id: http://rd1-1/subversion/SI/Tools/ynm/app/trunk@184173 7a6c5977-b700-0410-9ebf-f0490b269a9c

--- robot/setup/setting.py
+++ robot/setup/setting.py
@@ -1,22 +1,26 @@
 ip8155 = {
     'ip': '172.16.8.67',
     'mac': '00:AA:83:A5:00:15',
-    'user': 'root', 'passwd': ''
+    'user': 'root', 'passwd': '',
+    'platform': 'xarina'
 }
 fd9371htv = {
     'mac': '00:02:D1:3C:91:6B',
     'ip': '172.16.8.57',
-    'user': 'root', 'passwd': ''
+    'user': 'root', 'passwd': '',
+    'platform': 'hisillicon-standard'
 }
 ip9181 = {
     'mac': '00:02:FD:AA:13:26',
     'ip': '172.16.2.147',
-    'user': 'root', 'passwd': ''
+    'user': 'root', 'passwd': '',
+    'platform': 'hisillicon-standard'
 }
 vc8101 = {
     'mac': '00:02:D1:3D:36:C1',
     'ip': '172.16.7.201',
-    'user': 'root', 'passwd': ''
+    'user': 'root', 'passwd': '',
+    'platform': 'rossini-standard'
 }
 
 config = ip9181
