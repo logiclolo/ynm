09b3dff25a558fa2d241e951cfa59e04d3d66316
Stacey.Wu <Stacey.Wu@7a6c5977-b700-0410-9ebf-f0490b269a9c>
2016-03-31 11:23:36 +0000

    edge api v0.1
    git-svn-id: http://rd1-1/subversion/SI/Tools/ynm/app/trunk@184839 7a6c5977-b700-0410-9ebf-f0490b269a9c

--- YNM/camera/service/recording.py
+++ YNM/camera/service/recording.py
@@ -2,6 +2,21 @@
 
 import abc
 import requests
+import re
+from datetime import datetime
+
+
+def time_convert(timestr):
+
+    if type(timestr) is datetime:
+        return timestr
+
+    timestr = timestr.replace('T', ' ')
+    is_utc = 0
+    if re.search('Z', timestr) is not None:
+        is_utc = 1
+        timestr = timestr.replace('Z', '')
+    return datetime.strptime(timestr, '%Y-%m-%d %H:%M:%S.%f')
 
 
 class IHealth(object):
@@ -86,15 +101,32 @@ class ITrack(object):
         video/H264 '''
         raise NotImplemented()
 
-    @abc.abstractmethod
     def desdcription(self):
         ''' returns a text represents description '''
-        raise NotImplemented()
+        return self._description
 
-    @abc.abstractmethod
     def time(self):
         ''' returns a tuple which represents start/end time '''
-        raise NotImplemented()
+        return self._time
+
+
+class IFile(object):
+    __metaclass__ = abc.ABCMeta
+    ''' abstraction of a file '''
+
+    def path(self):
+        ''' returns a string represents file's path '''
+        return self._path
+
+    def is_locked(self):
+        ''' returns bool value '''
+        return self._is_locked
+
+    def time(self):
+        return self._time
+
+    def triggertype(self):
+        return self._triggertype
 
 
 class IRecordingItem(object):
@@ -149,13 +181,16 @@ class EdgeRecording (IStorage, IHealth):
 
     def __init__(self, cam):
         self._camera = cam
-        self._edgecgi = 'http://' + self._camera.url + '/cgi-bin/admin/lsctrlrec.cgi'
-        self._legacycgi = 'http://' + self._camera.url + '/cgi-bin/admin/lsctrl.cgi'
+        self._cgiurl = 'http://' + self._camera.url + '/cgi-bin'
+        self._edgecgi = self._cgiurl + '/admin/lsctrlrec.cgi'
+        self._legacycgi = self._cgiurl + '/admin/lsctrl.cgi'
+        self._fscgi = self._cgiurl + '/admin/getSDfilesystem.cgi'
         self._auth = (self._camera.username, self._camera.password)
         self._status = ''
         self._capacity = 0
         self._availablesize = 0
         self._usedsize = 0
+        self._filesystem = 'fat32'
 
         self._update_all_status()
 
@@ -163,7 +198,7 @@ class EdgeRecording (IStorage, IHealth):
         command = {'cmd': 'queryStatus'}
         r = requests.get(self._legacycgi, params=command, auth=self._auth)
         if r.status_code != 200:
-            raise Exception("Command edge error %s" % r)
+            raise Exception("Edge command (queryStatus) error %s" % r)
 
         x = xmltree.fromstring(r.content)
         self._status = x.find('disk/i0/cond').text
@@ -171,7 +206,37 @@ class EdgeRecording (IStorage, IHealth):
         self._availablesize = int(x.find('disk/i0/freespace').text)
         self._usedsize = int(x.find('disk/i0/usedspace').text)
 
-    # IStroage interface
+        r = requests.get(self._fscgi, auth=self._auth)
+        if r.status_code != 200:
+            raise Exception("Edge command (getSDfilesystem.cgi) error %s" % r)
+        self._filesystem = r.content.strip()
+
+    def _nativeapi_search(self, criteria):
+        command = {'cmd': 'getSlices',
+                   'recStartTime': criteria['starttime'],
+                   'recEndTime': criteria['endtime']}
+        if 'triggertype' in criteria:
+            command.update({'triggerType': ','.join(criteria['triggertype'])})
+
+        r = requests.get(self._edgecgi, params=command, auth=self._auth)
+        if r.status_code != 200:
+            raise Exception("Edge command (getSlices) error %s" % r)
+
+        x = xmltree.fromstring(r.content)
+        root = x.find('root')
+        slices = self.timelines(root)
+        return slices
+
+    def _legacy_search(self, criteria):
+        timeinterval = "BETWEEN '" + criteria['starttime'] + \
+                       "' AND '" + slice['endtime'] + "'"
+        command = {'cmd': 'search',
+                   'triggerTime': timeinterval}
+        r = requests.get(self._legacycgi, params=command, auth=self._auth)
+        x = xmltree.fromstring(r.content)
+
+    # IStorage interface
+
     def status(self):
         self._update_all_status()
         return self._status
@@ -194,11 +259,104 @@ class EdgeRecording (IStorage, IHealth):
         return self._camera.url + "/disk0"
 
     def filesystem(self):
-        raise NotImplemented()
-
-    def search(self):
         self._update_all_status()
-        raise NotImplemented()
+        return self._filesystem
+
+    def search(self, criteria):
+        if 'starttime' not in criteria:
+            criteria['starttime'] = '1970-01-01T00:00:00.000Z'
+        if 'endtime' not in criteria:
+            criteria['endtime'] = '2035-12-31T23:59:59.000Z'
+        if 'triggertype' in criteria:
+            if 'backup' in criteria['triggertype']:
+                criteria['backup'] = 1
+        if criteria['mediatype'] == 'videoclip':
+            return self._nativeapi_search(criteria)
+        else:
+            return self._legacy_search(criteria)
+
+    def timelines(self, xml_obj):
+        slices = dict()
+        idx = 0
+        for child in xml_obj:
+            if child.tag == 'slices':
+                slice = dict()
+                for et in child:
+                    if et.tag == 'sliceStart' or et.tag == 'sliceEnd':
+                        slice.update({et.tag: time_convert(et.text)})
+                    else:
+                        slice.update({et.tag: et.text})
+
+                # append tracks information
+                tracks = self.get_tracks(slice)
+                slice.update({'tracks': tracks})
+                # append files information
+                files = self.get_files(slice, mediatype='videoclip')
+                slice.update({'files': files})
+
+            slices.update({idx: slice})
+            idx += 1
+        return slices
+
+    def get_files(self, slice, mediaType="all"):
+
+        timeinterval = "BETWEEN '" + slice['sliceStart'] + \
+                       "' AND '" + slice['sliceEnd'] + "'"
+        command = {'cmd': 'search',
+                   'recToken': slice['recordingToken'],
+                   'triggerTime': timeinterval}
+        if mediaType == "videoclip":
+            command.update({'mediaType': mediaType})
+        r = requests.get(self._legacycgi, params=command, auth=self._auth)
+        x = xmltree.fromstring(r.content)
+        files = []
+        for idx in range(0, int(x.find('counts').text)):
+            info = x.find('i' + str(idx))
+            media_type = info.find('mediaType').text
+            label = info.find('label').text
+            trigger = info.find('triggerType').text
+            path = info.find('destPath').text
+            locked = info.find('isLocked').text
+            tri_time = info.find('triggerType').text
+            starttime = time_convert(info.find('beginTime').text)
+            endtime = time_convert(info.find('endTime').text)
+            time = (starttime, endtime)
+            if media_type == 'videoclip':
+                files.append(VideoFile(label, trigger,  path, locked, time))
+            elif media_type == 'text':
+                files.append(TextFile(label, trigger,  path, locked, tri_time))
+            elif media_type == 'snapshot':
+                files.append(SnapFile(label, trigger,  path, locked, tri_time))
+        return files
+
+    def get_tracks(self, slice):
+        command = {'cmd': 'getmediaattr',
+                   'recToken': slice['recordingToken'],
+                   'recTime': slice['sliceStart']}
+        r = requests.get(self._edgecgi, params=command, auth=self._auth)
+        x = xmltree.fromstring(r.content)
+
+        rec = x.find('root/recording/i0')
+        tracks = []
+        for idx in range(0, int(rec.find('trackNum').text)):
+            idx_track = 'trackAttributes/i' + str(idx)
+
+            info = rec.find(idx_track + '/trackInformation')
+            track_type = info.find('trackType').text.lower()
+            description = info.find('description').text
+            starttime = time_convert(info.find('dataFrom').text)
+            endtime = time_convert(info.find('dataTo').text)
+            time = (starttime, endtime)
+
+            attr = rec.find(idx_track + '/' + track_type + 'Attributes')
+            codec = attr.find('encoding').text
+
+            if track_type == 'video':
+                tracks.append(VideoTrack(codec, description, time))
+            elif track_type == 'audio':
+                tracks.append(AudioTrack(codec, description, time))
+
+        return tracks
 
     # IHealth interface
     def health(self):
@@ -209,5 +367,72 @@ class EdgeRecording (IStorage, IHealth):
         raise NotImplemented()
 
 
+class VideoTrack(ITrack):
+
+    def __init__(self, codec='H264', description=None, time=None):
+        self._codec = codec.upper()
+        self._description = description
+        self._time = time
+
+    def content_type(self):
+        return 'video/'+self._codec
+
+
+class AudioTrack(ITrack):
+
+    def __init__(self, codec='G711', description=None, time=None):
+        self._codec = codec.upper()
+        self._description = description
+        self._time = time
+
+    def content_type(self):
+        return 'audio/'+self._codec
+
+
+class VideoFile(IFile):
+
+    def __init__(self, label, trigger=None, path=None, locked=0, time=None):
+        self._label = label
+        self._trigger = trigger
+        self._path = path
+        self._time = time
+        self._is_locked = locked
+
+    def __repr__(self):
+        msg = "label: %s\t triggerType: %s\t path: %s\t" %\
+            (self._label, self._trigger, self._path)
+
+        if self._is_locked == '1':
+            msg += '\t locked'
+        return msg
+
+
+class TextFile(IFile):
+
+    def __init__(self, label, trigger=None, path=None, locked=0, time=None):
+        self._label = label
+        self._trigger = trigger
+        self._path = path
+        self._time = time
+        self._is_locked = locked
+
+
+class SnapFile(IFile):
+
+    def __init__(self, label, trigger=None, path=None, locked=0, time=None):
+        self._label = label
+        self._trigger = trigger
+        self._path = path
+        self._time = time
+        self._is_locked = locked
+
+
 IStorage.register(EdgeRecording)
 ITimeline.register(EdgeRecording)
+
+ITrack.register(VideoTrack)
+ITrack.register(AudioTrack)
+
+IFile.register(VideoFile)
+IFile.register(TextFile)
+IFile.register(SnapFile)
--- unit-test/all/unit-recording.py
+++ unit-test/all/unit-recording.py
@@ -1,11 +1,15 @@
 # -*- coding: utf-8 -*-
 
-from YNM.camera.service.recording import EdgeRecording
-from fixture import *
+from YNM.camera.service.recording import EdgeRecording, VideoTrack, AudioTrack
+from YNM.camera.service.recording import VideoFile, TextFile, SnapFile
+from YNM.camera.service.recording import time_convert
 import mock
+import pytest
 
 
-def test_edge_recording_status(cam):
+@pytest.mark.recording
+def test_edge_recording_status():
+    cam = mock.MagicMock()
     mget = mock.MagicMock()
     response = mock.MagicMock()
     response.content = '<?xml version="1.0" encoding="ISO-8859-1"?>\
@@ -31,5 +35,237 @@ def test_edge_recording_status(cam):
     assert edge_rec.location() == cam.url + '/disk0', "Storage location\
            incorrect, exp %s, act %s" % (cam.url + '/disk0',
                                          edge_rec.location())
+    p1.stop()
+
+
+def mock_recording_init():
+    cam = mock.MagicMock()
+    mget = mock.MagicMock()
+
+    status = mock.MagicMock()
+    status.status_code = 200
+    status.content = '<?xml version="1.0" encoding="ISO-8859-1"?>\
+        <stormgr version="1.0.9.20"><disk><i0><cond>detached</cond>\
+        <dbcond>notready</dbcond><totalsize>0</totalsize>\
+        <freespace>0</freespace><usedspace>0</usedspace></i0></disk>\
+        <statusCode>200</statusCode><statusString>OK</statusString></stormgr>'
+
+    mget.return_value = status
+    p1 = mock.patch('requests.get', new=mget)
+    p1.start()
+
+    edge_rec = EdgeRecording(cam)
+
+    p1.stop()
+    return edge_rec
+
+
+@pytest.mark.recording
+def test_edge_nativeapi_search():
+    mget = mock.MagicMock()
+    psr = mock.MagicMock()
+    getfiles = mock.MagicMock()
+
+    response = mock.MagicMock()
+    response.status_code = 200
+    response.content = '<?xml version="1.0" encoding="ISO-8859-1"?>\
+        <stormgr version="1.0.9.20"><statusCode>200</statusCode>\
+        <statusString>OK</statusString><root>\
+        <slices>\
+        <label>s1</label>\
+        <recordingToken>64079596710121389635</recordingToken>\
+        <islocked>0</islocked>\
+        <sliceStart>2016-02-07T14:44:42.000Z</sliceStart>\
+        <sliceEnd>2016-02-08T09:32:47.000Z</sliceEnd>\
+        </slices>\
+        <slices>\
+        <label>s2</label>\
+        <recordingToken>64079596710121389635</recordingToken>\
+        <islocked>0</islocked>\
+        <sliceStart>2016-02-15T06:34:04.206Z</sliceStart>\
+        <sliceEnd>2016-02-18T03:19:42.782Z</sliceEnd>\
+        </slices>\
+        </root></stormgr>'
+
+    tracks = []
+    time = ('2016-02-15T06:34:04.206Z', '2016-02-18T03:19:42.782Z')
+    tracks.append(VideoTrack('H265', 'videotest', time))
+    tracks.append(AudioTrack('G711', 'audiotest', time))
+
+    files = []
+    vpath = '/mnt/auto/CF/NCMF//20160215/14/IZ9361_rec-34.mp4'
+    spath = '/mnt/auto/CF/NCMF//20160215/14/ss1.jpg'
+    tpath = '/mnt/auto/CF/NCMF//20160215/14/abc.log'
+    files.append(VideoFile(1, 'seq', vpath, 0, time))
+    files.append(SnapFile(1, 'seq', spath))
+    files.append(TextFile(1, 'seq', tpath))
+
+    mget.return_value = response
+    psr.return_value = tracks
+    getfiles.return_value = files
+
+    edge = mock_recording_init()
+
+    p1 = mock.patch('requests.get', new=mget)
+    p2 = mock.patch(
+        'YNM.camera.service.recording.EdgeRecording.get_tracks', new=psr)
+    p3 = mock.patch(
+        'YNM.camera.service.recording.EdgeRecording.get_files', new=getfiles)
+
+    plist = [p1, p2, p3]
+
+    for p in plist:
+        p.start()
+
+    # search criteria 1
+    criteria = {
+        'mediatype': 'videoclip',
+        'triggertype': ['VADP', 'di'],
+        'starttime': '2016-02-01T12:34:56.789Z',
+        'endtime': '2016-02-19T12:00:00.000Z',
+    }
+    slices = edge.search(criteria)
+
+    for p in plist:
+        p.stop()
+
+    assert slices[0]['recordingToken'] == '64079596710121389635',\
+        "Search recTokn incorrect, exp '64079596710121389635', act '%s'"\
+        % slices[0]['recordingToken']
+
+    assert slices[0]['sliceStart'] == time_convert('2016-02-07T14:44:42.000Z'),\
+        "Search sliceStart incorrect, exp '%s', act '%s'"\
+        % (time_convert('2016-02-07T14:44:42.000Z'), slices[0]['sliceStart'])
+
+    assert slices[0]['tracks'][0].content_type() == 'video/H265',\
+        "Search tracks type incorrect, exp 'video/H265', act '%s'"\
+        % slices[0]['tracks'][0].content_type()
+
+    assert slices[0]['tracks'][1].content_type() == 'audio/G711',\
+        "Search tracks type incorrect, exp 'video/H265', act '%s'"\
+        % slices[0]['tracks'][1].content_type()
+
+    assert slices[0]['files'][0].path() == vpath,\
+        "Search tracks type incorrect, exp '%s', act '%s'"\
+        % (vpath, slices[0]['files'][0].path())
+
+    assert slices[0]['files'][1].path() == spath,\
+        "Search tracks type incorrect, exp '%s', act '%s'"\
+        % (spath, slices[0]['files'][1].path())
+
+
+@pytest.mark.recording
+def test_get_tracks():
+    res = mock.MagicMock()
+
+    res.return_value.status_code = 200
+    res.return_value.content = '<?xml version="1.0" encoding="ISO-8859-1"?>\
+        <stormgr version="1.0.9.20"><statusCode>200</statusCode>\
+        <statusString>OK</statusString>\
+        <root><recording><i0>\
+        <recordingToken>07327823181623539486</recordingToken>\
+        <from>2016-03-04T03:24:26.598Z</from>\
+        <until>2016-03-04T03:25:32.093Z</until>\
+        <backup>0</backup><trackAttributes>\
+        <i0><trackInformation><trackToken>a9486-28</trackToken>\
+        <trackType>Audio</trackType><description>Description</description>\
+        <dataFrom>2016-03-04T03:24:26.598Z</dataFrom>\
+        <dataTo>2016-03-04T03:25:32.093Z</dataTo></trackInformation>\
+        <audioAttributes><bitrate>3.15</bitrate><encoding>G711</encoding>\
+        <samplerate>8</samplerate></audioAttributes></i0>\
+        <i1><trackInformation><trackToken>v9486-29</trackToken>\
+        <trackType>Video</trackType><description>Description</description>\
+        <dataFrom>2016-03-04T03:24:26.598Z</dataFrom>\
+        <dataTo>2016-03-04T03:25:32.093Z</dataTo></trackInformation>\
+        <videoAttributes><bitrate>4538.00</bitrate>\
+        <width>600</width><height>800</height><encoding>H265</encoding>\
+        <framerate>29.90</framerate></videoAttributes></i1>\
+        </trackAttributes><trackNum>2</trackNum>\
+        </i0></recording><recordingNum>1</recordingNum></root></stormgr>'
+
+    edge = mock_recording_init()
+
+    p1 = mock.patch('requests.get', new=res)
+    p1.start()
+
+    slice = {
+        'sliceStart': '2016-03-04T03:24:26.598Z',
+        'sliceEnd': '2016-03-04T03:25:32.093Z',
+        'recordingToken': '07327823181623539486'
+    }
+
+    command = {'cmd': 'getmediaattr',
+               'recToken': slice['recordingToken'],
+               'recTime': slice['sliceStart']}
+    tracks = edge.get_tracks(slice)
+    url = edge._edgecgi
+    res.assert_called_once_with(url, params=command, auth=edge._auth)
+
+    p1.stop()
+
+    starttime = time_convert(slice['sliceStart'])
+    endtime = time_convert(slice['sliceEnd'])
+    assert tracks[0].content_type() == 'audio/G711',\
+        "Search tracks type incorrect, exp 'audio/G711', act '%s'"\
+        % tracks[0].content_type()
+    assert tracks[0].time() == (starttime, endtime),\
+        "Search tracks type incorrect, exp '(%s, %s)', act '%s'"\
+        % (starttime, endtime, tracks[0].time())
+    assert tracks[1].content_type() == 'video/H265',\
+        "Search tracks type incorrect, exp 'video/H265', act '%s'"\
+        % tracks[1].content_type()
+    assert tracks[1].time() == (starttime, endtime),\
+        "Search tracks type incorrect, exp '(%s, %s)', act '%s'"\
+        % (starttime, endtime, tracks[1].time())
+
+
+@pytest.mark.recording
+def test_get_files():
+    res = mock.MagicMock()
+    res.return_value.status_code = 200
+    res.return_value.content = '<?xml version="1.0" encoding="ISO-8859-1"?>\
+    <stormgr version="1.0.9.20"><counts>2</counts>\
+    <i0><label>2</label><triggerType>seq</triggerType>\
+    <mediaType>videoclip</mediaType>\
+    <destPath>/mnt/auto/CF/NCMF//20160215/14/IZ9361_rec-34.mp4</destPath>\
+    <resolution>1920x1080</resolution><isLocked>0</isLocked>\
+    <triggerTime>2016-02-15 14:34:04.206</triggerTime>\
+    <beginTime>2016-02-15 14:34:04.206</beginTime>\
+    <endTime>2016-02-15 14:44:04.055</endTime>\
+    <dst>0</dst><tz>320</tz><backup>0</backup><seamless>0</seamless></i0>\
+    <i1><label>3</label><triggerType>seq</triggerType>\
+    <mediaType>videoclip</mediaType>\
+    <destPath>/mnt/auto/CF/NCMF//20160215/14/IZ9361_rec-44.mp4</destPath>\
+    <resolution>1920x1080</resolution><isLocked>0</isLocked>\
+    <triggerTime>2016-02-15 14:44:04.055</triggerTime>\
+    <beginTime>2016-02-15 14:44:04.055</beginTime>\
+    <endTime>2016-02-15 14:54:04.320</endTime>\
+    <dst>0</dst><tz>320</tz><backup>0</backup><seamless>0</seamless></i1>\
+    <statusCode>200</statusCode><statusString>OK</statusString></stormgr>'
+
+    edge = mock_recording_init()
+
+    p1 = mock.patch('requests.get', new=res)
+    p1.start()
+
+    slice = {
+        'recordingToken': '07327823181623539486',
+        'sliceStart': '2016-02-15 14:34:04.206',
+        'sliceEnd': '2016-02-15 14:54:04.320'
+    }
+
+    files = edge.get_files(slice, mediaType="videoclip")
+
+    starttime = time_convert(slice['sliceStart'])
+    middletime = time_convert('2016-02-15 14:44:04.055')
+    endtime = time_convert(slice['sliceEnd'])
+
+    assert files[0].time() == (starttime, middletime),\
+        "Search files incorrect, exp '(%s, %s)', act '%s'"\
+        % (middletime, endtime, files[0].time())
+
+    assert files[1].time() == (middletime, endtime),\
+        "Search files incorrect, exp '(%s, %s)', act '%s'"\
+        % (middletime, endtime, files[1].time())
 
     p1.stop()
